57ff8115d2c3aae6da62dc2afe6586865964502d
Adriano Ribeiro <adrianolucianoribeiro@gmail.com>
2018-11-19 20:31:49 -0200

    Adicionando workers e redis

---
+++ worker/worker.py
@@ -0,0 +1,17 @@
+import redis
+import json
+from time import sleep
+from random import randint
+
+
+if __name__ == '__main__':
+    print('Iniciando worker')
+    r = redis.Redis(host='queue', port=6379, db=0)
+
+    while True:
+        print('Ouvindo mensagem')
+        mensagem = json.loads(r.blpop('sender')[1])
+        # Simulando envio de email...
+        print('Mandando a mensagem: ', mensagem['assunto'])
+        sleep(randint(5, 20))
+        print('Mensagem', mensagem['assunto'], 'enviada')
